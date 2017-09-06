a7850b678990da4c6ac04b6290113a00310106f3
Pavel <shirokuiuna@mail.ru>
2017-09-06 14:58:54 +0300

    upload mishka project

---
+++ markup/components/head/data/data.js
@@ -0,0 +1,14 @@
+head: {
+    defaults: {
+        title: 'Главная страница',
+        useSocialMetaTags: true
+    },
+    "catalog": {
+      title: 'Каталог',
+      useSocialMetaTags: true
+    },
+    "form": {
+      title: 'Вязание на заказ',
+      useSocialMetaTags: true
+    }
+}
---
+++ markup/static/js/libraries/jquery-code.js
@@ -0,0 +1,25 @@
+$(function () {
+  $('#mainNavButton').click(function () {
+    $('#mainNav').toggleClass('main-nav--open');
+  });
+  
+  $("#reviewsSlider").owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay: false,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    smartSpeed: 1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+  });
+});
